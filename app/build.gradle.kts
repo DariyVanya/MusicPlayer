a7c1ad2c726e91ede37fbe84e34cf7c3ff7a60f7
DariyVanya <dariyvanya633@gmail.com>
2024-12-11 16:30:39 +0200

    first stable version of using exoplayer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation("com.google.android.exoplayer:exoplayer:2.16.1")
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,20 +1,75 @@
 package com.example.musicplayer
 
+
+import android.media.session.PlaybackState
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.MediaMetadata
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.PlayerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Player.Listener{
+
+    private lateinit var player: ExoPlayer
+    private lateinit var playerView: PlayerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var titleTV: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        progressBar = findViewById(R.id.progressBar)
+        titleTV = findViewById(R.id.title)
+
+        setupPlayer()
+        addMp3Files()
+        addMp4Files()
+    }
+
+    private fun setupPlayer(){
+        player = ExoPlayer.Builder(this).build()
+        playerView = findViewById(R.id.video_view)
+        playerView.player = player
+        player.addListener(this)
+    }
+
+    private fun addMp4Files(){
+        val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
+        player.addMediaItem(mediaItem)
+        player.prepare()
+    }
+
+    private fun addMp3Files(){
+        val mediaItem = MediaItem.fromUri(getString(R.string.test_mp3))
+        player.addMediaItem(mediaItem)
+        player.prepare()
+    }
+
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        super.onPlaybackStateChanged(playbackState)
+
+        when (playbackState){
+            Player.STATE_BUFFERING -> {
+                progressBar.visibility = View.VISIBLE
+            }
+            Player.STATE_READY -> {
+                progressBar.visibility = View.INVISIBLE
+            }
+
         }
+    }
+
+    override fun onMediaMetadataChanged(mediaMetadata: MediaMetadata) {
+        super.onMediaMetadataChanged(mediaMetadata)
+        titleTV.text = mediaMetadata.title ?: mediaMetadata.displayTitle ?: "Title not found"
     }
 }
