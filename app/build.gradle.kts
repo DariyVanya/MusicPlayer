70500221473201e2564518ea6a8f91ea1ae8ddc8
dariy <dariyvanya633@gmail.com>
2024-12-17 18:52:30 +0200

    remade player

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // https://mvnrepository.com/artifact/androidx.work/work-runtime-ktx
+    runtimeOnly("androidx.work:work-runtime-ktx:2.7.1")
 }
--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,95 +1,82 @@
 package com.example.musicplayer
 
 
-import android.media.session.PlaybackState
+import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import android.os.Bundle
-import android.view.View
-import android.widget.ProgressBar
+import android.os.Handler
+import android.widget.ImageButton
+import android.widget.SeekBar
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.MediaMetadata
-import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.ui.PlayerView
 
-class MainActivity : AppCompatActivity(), Player.Listener{
 
-    private lateinit var player: ExoPlayer
-    private lateinit var playerView: PlayerView
-    private lateinit var progressBar: ProgressBar
-    private lateinit var titleTV: TextView
+class MainActivity : AppCompatActivity(){
 
+    lateinit var runnable: Runnable
+    private var handler = Handler()
+
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        progressBar = findViewById(R.id.progressBar)
-        titleTV = findViewById(R.id.title)
+        setContentView(R.layout.player_fs)
 
-        setupPlayer()
-        addMp3Files()
-        addMp4Files()
+        //Creating a mediaPlayer for music.mp3
+        val mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.music)
 
-        if (savedInstanceState != null){
-            savedInstanceState.getInt("mediaItem").let{ restoredMedia ->
-                val seekTime = savedInstanceState.getLong("SeekTime")
-                player.seekTo(restoredMedia, seekTime)
-                player.play()
+        //seekBar
+        val seekBar: SeekBar = findViewById(R.id.soundtrack_seekBar)
+        seekBar.progress = 0
+        seekBar.max = mediaPlayer.duration
+
+        //Current and max duration textView
+        val currentText: TextView = findViewById(R.id.progress_current)
+        val maxText: TextView = findViewById(R.id.progress_max)
+        maxText.
+
+        // play listener
+        val playBtn: ImageButton = findViewById(R.id.play_btn)
+
+
+
+        playBtn.setOnClickListener{
+            if (!mediaPlayer.isPlaying) {
+                mediaPlayer.start()
+                playBtn.setImageResource(R.drawable.baseline_pause_24)
+
+            } else {
+                mediaPlayer.pause()
+                playBtn.setImageResource(R.drawable.baseline_play_arrow_24)
             }
         }
-    }
-
-    private fun setupPlayer(){
-        player = ExoPlayer.Builder(this).build()
-        playerView = findViewById(R.id.video_view)
-        playerView.player = player
-        player.addListener(this)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-
-        outState.putLong("SeekTime", player.currentPosition)
-        outState.putInt("mediaItem", player.currentMediaItemIndex)
-    }
-
-    private fun addMp4Files(){
-        val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
-        player.addMediaItem(mediaItem)
-        player.prepare()
-    }
-
-    private fun addMp3Files(){
-        val mediaItem = MediaItem.fromUri(getString(R.string.test_mp3))
-        player.addMediaItem(mediaItem)
-        player.prepare()
-    }
-
-    override fun onPlaybackStateChanged(playbackState: Int) {
-        super.onPlaybackStateChanged(playbackState)
-
-        when (playbackState){
-            Player.STATE_BUFFERING -> {
-                progressBar.visibility = View.VISIBLE
-            }
-            Player.STATE_READY -> {
-                progressBar.visibility = View.INVISIBLE
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, changed: Boolean) {
+                if (changed){
+                    mediaPlayer.seekTo (progress)
+                }
             }
 
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+        runnable = Runnable {
+            seekBar.progress = mediaPlayer.currentPosition
+            handler.postDelayed (runnable, 1000)
         }
-    }
+        handler.postDelayed(runnable, 1000)
 
-    override fun onStop() {
-        super.onStop()
-        player.release()
-    }
+        mediaPlayer.setOnCompletionListener {
+            playBtn.setImageResource(R.drawable.baseline_play_arrow_24)
+            seekBar.progress = 0
+        }
 
-    override fun onMediaMetadataChanged(mediaMetadata: MediaMetadata) {
-        super.onMediaMetadataChanged(mediaMetadata)
-        titleTV.text = mediaMetadata.title ?: mediaMetadata.displayTitle ?: "Title not found"
+
     }
 }
