ef6f344a6c57f3b03c6830de3a92b98f06e3175b
DariyVanya <dariyvanya633@gmail.com>
2024-12-18 14:07:29 +0200

    added playlist and event for opening player_fs.xml

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.musicplayer
 
 
 import android.annotation.SuppressLint
+import android.media.Image
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
+import java.sql.Time
+import java.util.concurrent.TimeUnit
+import kotlin.time.toDuration
 
 
 class MainActivity : AppCompatActivity(){
@@ -16,13 +22,16 @@ class MainActivity : AppCompatActivity(){
     lateinit var runnable: Runnable
     private var handler = Handler()
 
+    fun getTimeDuration(time:Int) : String{
+        val res: String = String.format("%02d:%02d",
+            TimeUnit.MILLISECONDS.toMinutes(time.toLong()),
+            TimeUnit.MILLISECONDS.toSeconds(time.toLong()) -
+                    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(time.toLong())))
+        return res
+    }
 
-
-    @SuppressLint("MissingInflatedId")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    fun goToPlayer(){
         setContentView(R.layout.player_fs)
-
         //Creating a mediaPlayer for music.mp3
         val mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.music)
 
@@ -35,10 +44,17 @@ class MainActivity : AppCompatActivity(){
         val currentText: TextView = findViewById(R.id.progress_current)
         val maxText: TextView = findViewById(R.id.progress_max)
 
+        maxText.text = getTimeDuration(mediaPlayer.duration)
+
+
         // play listener
         val playBtn: ImageButton = findViewById(R.id.play_btn)
+        val backBtn: ImageButton = findViewById(R.id.go_back_btn)
 
 
+        backBtn.setOnClickListener{
+            goToPlaylist()
+        }
 
         playBtn.setOnClickListener{
             if (!mediaPlayer.isPlaying) {
@@ -68,14 +84,40 @@ class MainActivity : AppCompatActivity(){
         runnable = Runnable {
             seekBar.progress = mediaPlayer.currentPosition
             handler.postDelayed (runnable, 1000)
+            currentText.text = getTimeDuration(mediaPlayer.currentPosition)
         }
         handler.postDelayed(runnable, 1000)
 
         mediaPlayer.setOnCompletionListener {
+            mediaPlayer.seekTo (0)
             playBtn.setImageResource(R.drawable.baseline_play_arrow_24)
             seekBar.progress = 0
+            currentText.text = "00:00"
+            maxText.text = "00:00"
+        }
+    }
+
+    fun goToPlaylist() {
+        setContentView(R.layout.playlist)
+
+        val imageView: ImageView = findViewById(R.id.soundtrack_cover_img_playlist)
+        val titleText: TextView = findViewById(R.id.soundtrack_name)
+
+        imageView.setOnClickListener{
+            goToPlayer()
         }
 
+        titleText.setOnClickListener{
+            goToPlayer()
+        }
+
+    }
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        goToPlaylist()
 
     }
 }
